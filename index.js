ff62d89c1f4e29a5926e7d694f8ed74a19f74453
Enmanuel Ferrer <enmanuelferrer2511@gmail.com>
2025-10-02 15:00:57 -0400

    feat: add stylelint config and export in main module

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  eslint: require('./eslint.config.js'),
+  prettier: require('./prettier.config.js'),
+  lintStaged: require('./lint-staged.config.js'),
+  stylelint: require('./stylelint.config.js'),
+};
